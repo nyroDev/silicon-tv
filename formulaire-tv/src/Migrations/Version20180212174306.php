128318503304bd331f4674e4a48114b3bae14ec1
Nyro <cedric@nyrodev.com>
2018-02-12 20:18:26 +0100

    Fix last visual issues (#2)
    * Fix last issues
    
    * Add migrations vendors
    
    * Revert back delete
    
    * Fix last visual issues and form email_hidden
    
    * Fix erronous assets URL

--- formulaire-tv/src/Migrations/Version20180212174306.php
+++ formulaire-tv/src/Migrations/Version20180212174306.php
@@ -8,14 +8,14 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180212094300 extends AbstractMigration
+class Version20180212174306 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE member (id INT UNSIGNED AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, name VARCHAR(200) NOT NULL, logo_name VARCHAR(255) NOT NULL, image_name VARCHAR(255) NOT NULL, image_size INT NOT NULL, updated_at DATETIME NOT NULL, bio LONGTEXT NOT NULL, url VARCHAR(255) DEFAULT NULL, facebook VARCHAR(255) DEFAULT NULL, twitter VARCHAR(255) DEFAULT NULL, instagram VARCHAR(255) DEFAULT NULL, linkedin VARCHAR(255) DEFAULT NULL, valid TINYINT(1) DEFAULT \'1\' NOT NULL, UNIQUE INDEX UNIQ_70E4FA78E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE member (id INT UNSIGNED AUTO_INCREMENT NOT NULL, email VARCHAR(255) NOT NULL, email_hidden TINYINT(1) DEFAULT \'0\' NOT NULL, name VARCHAR(200) NOT NULL, logo_name VARCHAR(255) NOT NULL, logo_size INT NOT NULL, image_name VARCHAR(255) DEFAULT NULL, image_size INT DEFAULT NULL, updated_at DATETIME NOT NULL, bio LONGTEXT NOT NULL, url VARCHAR(255) DEFAULT NULL, facebook VARCHAR(255) DEFAULT NULL, twitter VARCHAR(255) DEFAULT NULL, instagram VARCHAR(255) DEFAULT NULL, linkedin VARCHAR(255) DEFAULT NULL, valid TINYINT(1) DEFAULT \'1\' NOT NULL, UNIQUE INDEX UNIQ_70E4FA78E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema)

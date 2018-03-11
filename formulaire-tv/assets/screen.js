92e88fd09f922f0581af660cf0535bb9bf2aca2a
nyro <cedric@nyrodev.com>
2018-03-11 01:42:52 +0100

    Fix last screen anims

--- formulaire-tv/assets/screen.js
+++ formulaire-tv/assets/screen.js
@@ -2,20 +2,19 @@ var displayTime = 20;
 
 var body = document.querySelector('body'),
     main = document.querySelector('main'),
-    articles = document.querySelector('article'),
+    articles = document.querySelectorAll('article'),
     articlesNb = articles.length,
     curShown = 0,
     nextShown,
     state;
 
 // Start by shuffling all articles
-var articlesTmp = document.querySelector('article');
 for (var i = articlesNb; i >= 0; i--) {
     main.appendChild(main.children[Math.random() * i | 0]);
 }
 
 // Get it back again shuffled
-articles = document.querySelector('article');
+articles = document.querySelectorAll('article');
 
 // Show the first one
 articles[curShown].classList.add('shown');
@@ -32,6 +31,7 @@ body.addEventListener('transitionend', function() {
             // Prepare the next one in reverse side
             articles[nextShown].classList.add('nextShown');
             articles[nextShown].classList.add('shown');
+            
             main.classList.add('anim');
         });
     } else {
@@ -42,9 +42,13 @@ body.addEventListener('transitionend', function() {
 
 main.addEventListener('transitionend', function(event) {
     event.stopPropagation();
+    if (state === 'end') {
+        return;
+    }
     requestAnimationFrame(function() {
         // Rotate animation is done, swipe article and zoom again
 
+        state = 'end';
         main.classList.add('noAnim');
         main.classList.remove('anim');
 
@@ -52,11 +56,12 @@ main.addEventListener('transitionend', function(event) {
         articles[nextShown].classList.remove('nextShown');
         curShown = nextShown;
 
-        main.classList.remove('noAnim');
-
-        state = 'end';
-        // Zoom body again
-        body.classList.remove('anim');
+        requestAnimationFrame(function() {
+            main.classList.remove('noAnim');
+    
+            // Zoom body again
+            body.classList.remove('anim');
+        });
     });
 });
 

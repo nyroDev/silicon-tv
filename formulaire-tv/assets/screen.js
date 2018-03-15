8eb7e3a2167748a5de2fab2230627cb12cda1b5b
nyro <cedric@nyrodev.com>
2018-03-15 08:55:59 +0100

    ClearTimeout when starting

--- formulaire-tv/assets/screen.js
+++ formulaire-tv/assets/screen.js
@@ -1,24 +1,25 @@
 var displayTime = 20;
 
 var body = document.querySelector('body'),
-    main = document.querySelector('main'),
-    articles = document.querySelectorAll('article'),
-    articlesNb = articles.length,
-    curShown = 0,
-    nextShown,
-    state = false;
+	main = document.querySelector('main'),
+	articles = document.querySelectorAll('article'),
+	articlesNb = articles.length,
+	curShown = 0,
+	nextShown,
+	curTimeout,
+	state = false;
 
 if ('serviceWorker' in navigator && body.dataset.sw) {
-    navigator.serviceWorker.register(body.dataset.sw, {
-      scope: '/'
-    }).then(function(reg) {
-        console.log('service worker ready');
-    });
+	navigator.serviceWorker.register(body.dataset.sw, {
+		scope: '/'
+	}).then(function (reg) {
+		console.log('service worker ready');
+	});
 }
 
 // Start by shuffling all articles
 for (var i = articlesNb; i >= 0; i--) {
-    main.appendChild(main.children[Math.random() * i | 0]);
+	main.appendChild(main.children[Math.random() * i | 0]);
 }
 
 // Get it back again shuffled
@@ -28,68 +29,72 @@ articles = document.querySelectorAll('article');
 articles[curShown].classList.add('shown');
 
 // Prepare all events listener
-body.addEventListener('transitionend', function() {
-    if (state === 'start') {
-        requestAnimationFrame(function() {
-            // Select the nextOne
-            nextShown = curShown + 1;
-            if (nextShown === articlesNb) {
-                nextShown = 0;
-            }
-            // Prepare the next one in reverse side
-            articles[nextShown].classList.add('nextShown');
-            articles[nextShown].classList.add('shown');
+body.addEventListener('transitionend', function () {
+	if (state === 'start') {
+		requestAnimationFrame(function () {
+			// Select the nextOne
+			nextShown = curShown + 1;
+			if (nextShown === articlesNb) {
+				nextShown = 0;
+			}
+			// Prepare the next one in reverse side
+			articles[nextShown].classList.add('nextShown');
+			articles[nextShown].classList.add('shown');
 
-            main.classList.add('anim');
-        });
-    } else {
-        // Start a new display
-        state = false;
-        display();
-    }
+			main.classList.add('anim');
+		});
+	} else {
+		// Start a new display
+		state = false;
+		display();
+	}
 });
 
-main.addEventListener('transitionend', function(event) {
-    event.stopPropagation();
-    if (state === 'end') {
-        return;
-    }
-    requestAnimationFrame(function() {
-        // Rotate animation is done, swipe article and zoom again
+main.addEventListener('transitionend', function (event) {
+	event.stopPropagation();
+	if (state === 'end') {
+		return;
+	}
+	requestAnimationFrame(function () {
+		// Rotate animation is done, swipe article and zoom again
 
-        state = 'end';
-        main.classList.add('noAnim');
-        main.classList.remove('anim');
+		state = 'end';
+		main.classList.add('noAnim');
+		main.classList.remove('anim');
 
-        articles[curShown].classList.remove('shown');
-        articles[nextShown].classList.remove('nextShown');
-        curShown = nextShown;
+		articles[curShown].classList.remove('shown');
+		articles[nextShown].classList.remove('nextShown');
+		curShown = nextShown;
 
-        requestAnimationFrame(function() {
-            main.classList.remove('noAnim');
+		requestAnimationFrame(function () {
+			main.classList.remove('noAnim');
 
-            // Zoom body again
-            body.classList.remove('anim');
-        });
-    });
+			// Zoom body again
+			body.classList.remove('anim');
+		});
+	});
 });
 
-body.addEventListener('keydown', function(event) {
-    event = event || window.event;
-    if (event.keyCode === 39 && !state) {
-        startAnim();
-    }
+body.addEventListener('keydown', function (event) {
+	event = event || window.event;
+	if (event.keyCode === 39 && !state) {
+		startAnim();
+	}
 });
 
 function startAnim() {
-    requestAnimationFrame(function() {
-        state = 'start';
-        body.classList.add('anim');
-    });
+	if (curTimeout) {
+		clearTimeout(curTimeout);
+    curTimeout = false;
+	}
+	requestAnimationFrame(function () {
+		state = 'start';
+		body.classList.add('anim');
+	});
 }
 
 function display() {
-    setTimeout(startAnim, displayTime * 1000);
+	curTimeout = setTimeout(startAnim, displayTime * 1000);
 }
 
 // Start display
